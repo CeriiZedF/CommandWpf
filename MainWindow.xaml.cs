04f79c4056a7543c7a61f345f8795402d7bcd0e4
CeriiZedF <dasbalan14@gmail.com>
2023-07-08 18:04:17 +0300

    -

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,18 +27,35 @@ namespace CommandWpf
         public MainWindow()
         {
             InitializeComponent();
-
+            
             
         }
 
-        public void SetBackground(SolidColorBrush s)
-        {
-            this.Background = s;
-        }
+        
     }
 
     public class MyViewModel : INotifyPropertyChanged
     {
+        private int font_size;
+        public int FontSize
+        { 
+            get { return font_size; } 
+            set 
+            {
+                font_size = value;
+                OnPropertyChanged("FontSize");
+            }
+        }
+        private string font_family;
+        public string Font
+        {
+            get { return font_family; }
+            set
+            {
+                font_family = value;
+                OnPropertyChanged("Font");
+            }
+        }
         private SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
         public SolidColorBrush myBrush
             {
@@ -85,6 +102,70 @@ namespace CommandWpf
                     ?? (greenCommand = new ActionCommand(obj =>
                     {
                         myBrush = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+                    }));
+            }
+
+        }
+
+        private ActionCommand fontBold;
+        public ActionCommand FontBold
+        {
+            get
+            {
+                return fontBold
+                    ?? (fontBold = new ActionCommand(obj =>
+                    {
+                        MessageBox.Show("Bold");
+                        font_family = "Bold";
+                        
+                    }));
+            }
+
+        }
+        private ActionCommand fontItalic;
+        public ActionCommand FontItalic
+        {
+            get
+            {
+                return fontItalic
+                    ?? (fontItalic = new ActionCommand(obj =>
+                    {
+                        font_family = "Italic";
+                        MessageBox.Show(font_family.ToString());
+                    }));
+            }
+
+        }
+        private ActionCommand fontSize15;
+        public ActionCommand FontSize15
+        {
+            get
+            {
+                return fontSize15
+                    ?? (fontSize15 = new ActionCommand(obj =>
+                    {
+                        MessageBox.Show("FontSize 15");
+                        FontSize = 15;
+                        
+
+
+                    }));
+            }
+
+        }
+        private ActionCommand fontSize30;
+        public ActionCommand FontSize30
+        {
+            get
+            {
+                return fontSize30
+                    ?? (fontSize30 = new ActionCommand(obj =>
+                    {
+                        MessageBox.Show("FontSize 15");
+                        FontSize = 30;
+
+
+
                     }));
             }
 
